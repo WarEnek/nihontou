d2448f25ebb77319d32167a9a9c00df1ae1c61b9
Danil Efremov <warenek@gmail.com>
2024-07-09 15:07:48 +0400

    fix config

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
 import svgr from "vite-plugin-svgr";
 
 export default defineConfig({
-	plugins: [react(), TanStackRouterVite(), svgr()],
+	plugins: [react(), svgr()],
 	server: {
 		proxy: {
 			"/api": {
